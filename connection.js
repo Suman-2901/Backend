a21dbac6ad4f70d5fed69884a779d75eaf85d881
Suman-2901 <suma230102060@iiitmanipur.ac.in>
2024-08-21 16:40:38 +0530

    submit

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 const port = process.env.port||3000;
@@ -9,7 +9,12 @@ const db_host=process.env.db_host;
 const db_user= process.env.db_user;
 const db_password= process.env.db_password;
 const db= process.env.db;
+const sql2=process.env.qry2;
+const sql1=process.env.qry1;
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
     host:db_host ,
@@ -30,7 +35,7 @@ connection.connect(err => {
 
 // Endpoint to get items
 app.get('/items', (req, res) => {
-    connection.query('SELECT * FROM product', (err, results) => {
+    connection.query(sql1, (err, results) => {
         if (err) {
             console.error('Error fetching items:', err);
             res.status(500).send('Error fetching items');
@@ -39,7 +44,16 @@ app.get('/items', (req, res) => {
         res.json(results);
     });
 });
-
+app.post('/submit', (req, res) => {
+    const quantity = parseInt(req.body.Quantity, 10);
+    const price = parseFloat(req.body.Price,10);
+    const { category, search} = req.body;
+    console.log(req.body.Quantity);
+    connection.query(sql2, [category, search, quantity, price], (err, result) => {
+        if (err) throw err;
+        res.send('Product added to database');
+    });
+});
 // // Start the server
 app.listen(port, () => {
     console.log(`Server running on port${port}`);
